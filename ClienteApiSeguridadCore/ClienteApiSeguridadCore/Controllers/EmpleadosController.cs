f6248f1635df9bbe542fb47bd9c680cff38a7d11
David <davidvalencia_@hotmail.com>
2019-03-15 14:28:44 +0100

    Version 19.3.15.14

---
+++ ClienteApiSeguridadCore/ClienteApiSeguridadCore/Controllers/EmpleadosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClienteApiSeguridadCore.Filters;
+using ClienteApiSeguridadCore.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClienteApiSeguridadCore.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        IRepositoryApiEmpleados repo;
+        public EmpleadosController(IRepositoryApiEmpleados repo)
+        {
+            this.repo = repo;
+        }
+
+        [EmpleadosAuthorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
